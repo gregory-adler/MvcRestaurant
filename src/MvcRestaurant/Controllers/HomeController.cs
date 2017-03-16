eb38d8d6d145608149d299a8470575ecd4d7944f
Gregory Adler <greg7239@gmail.com>
2017-03-16 19:00:54 -0400

    Hello World Mvc Restaurant

---
+++ src/MvcRestaurant/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcRestaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "MvcRestauarant";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This was made as a project to help me develop my asp .net skills";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Gregory Adler";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
