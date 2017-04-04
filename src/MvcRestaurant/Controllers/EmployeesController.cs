9119301d0c40a3aff32d8cbd009f463af64688cf
Gregory Adler <greg7239@gmail.com>
2017-04-04 17:38:54 -0400

    Cleaned up code - added employees controller and view

--- src/MvcRestaurant/Controllers/EmployeesController.cs
+++ src/MvcRestaurant/Controllers/EmployeesController.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MvcRestaurant.Models;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MvcRestaurant.Controllers
 {
-    public class CreateRestaurant : Controller
+    public class EmployeesController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            Server test = new Server("the");
+            ViewData["Title"] = "Employees";
             return View();
         }
     }
--- src/MvcRestaurant/Models/Server.cs
+++ src/MvcRestaurant/Models/Server.cs
@@ -7,7 +7,6 @@ namespace MvcRestaurant.Models
 {
     public class Server
     {
-
         public int ServerId { get; set; }
         public string name { get; set; }
         public int age { get; set; }
