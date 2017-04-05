07857dd61a405ced9e5c8799418b4a08a7628a38
Gregory Adler <greg7239@gmail.com>
2017-04-05 17:01:36 -0400

    added EmployeesRepository

---
+++ src/MvcRestaurant/Models/EmployeesRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcRestaurant.Data;
+
+namespace MvcRestaurant.Models
+{
+    public class EmployeesRepository : IEmployeesRepository
+    {
+        public ApplicationDbContext db;
+        public EmployeesRepository(ApplicationDbContext database)
+        {
+            db = database;
+        }
+        public async Task<IList<Employee>> getEmployees()
+        {
+            return null;
+        }
+    }
+}
