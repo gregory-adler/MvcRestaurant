2443a9817e67b419f25d46ef9a01b4e817181561
Gregory Adler <greg7239@gmail.com>
2017-04-05 16:49:07 -0400

    Added Employees irepository and view

--- src/MvcRestaurant/Controllers/EmployeesController.cs
+++ src/MvcRestaurant/Controllers/EmployeesController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcRestaurant.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcRestaurant.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly IEmployeesRepository _employeesRepository;
+        public EmployeesController(IEmployeesRepository employeesRepository)
+        {
+            _employeesRepository = employeesRepository;
+        }
+
         public IActionResult Index()
         {
             ViewData["Title"] = "Employees";
---
+++ src/MvcRestaurant/Models/IEmployeesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcRestaurant.Models
+{
+    public interface IEmployeesRepository
+    {
+        Task<IList<Employee>> getEmployees();
+    }
+}
